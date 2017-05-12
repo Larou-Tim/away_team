b4ac59a5a7297c17878e4573bac8919e0112b6b6
Tim LaRou <larou.tim@gmail.com>
2017-05-11 21:39:20 -0500

    heroku update

--- app/models/index.js
+++ app/models/index.js
@@ -8,11 +8,20 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+var sequelize = new Sequelize("postgres://zrzoizidwqmugv:b4fb6416ed081d93a297e4fc49e0984eeecfb1555315b3f276029220310fa68d@ec2-54-235-120-39.compute-1.amazonaws.com:5432/d6qbip4gnueudf",
+{
+  dialect:"postgres",
+  protocol:"postgres",
+  dialectOptions:{
+    ssl: true
+  }
+})
 
 fs
   .readdirSync(__dirname)
